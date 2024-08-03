6a8903abbbdf02185f763c7dba9303afec4c3698
ronit127 <00ronitr@gmail.com>
2024-08-03 18:48:21 -0500

    added actual question properties: what the question is, option is and the answer to the Node

--- node.py
+++ node.py
@@ -2,9 +2,12 @@ from typing import List
 
 class Node:
     #node contains name(string), skills(list of strings), difficulty(double)
-    def __init__(self, key: int = None, name: str = None, skills: List[str] = None, difficulty: float = None, probability: float = None) -> None:
+    def __init__(self, key: int = None, name: str = None, skills: List[str] = None, difficulty: float = None, question: str = None, options: List[str] = None, answer: str = None) -> None:
         self.name = name
-        self.skills = skills
+        if skills is None:
+            skills = []
+        else:
+            self.skills = skills
         self.difficulty = difficulty
         self.key = key
 
